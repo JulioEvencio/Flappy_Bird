53b66ec2189399700ebf5c263826d6a6a6b9e510
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-30 21:39:56 -0300

    Funções que iniciam e finalizam a SDL foram adicionadas

--- Flappy Bird/main.c
+++ Flappy Bird/main.c
@@ -4,11 +4,18 @@
 
 //  Bibliotecas
 #include "cabecalho.h"
+#include "sdl2/sdl2.c"
 #include "funcoes.c"
 
 int main(int argc, char *args[])
 {
+    //  Iniciando SDL, IMG e TTF
+    sdl2_iniciar_SDL2();
+
     //  Code
+
+    //  Finalizando SDL, IMG e TTF
+    sdl2_finalizar_SDL2();
 
     return 0;
 }

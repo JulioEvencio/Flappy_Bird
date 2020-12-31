73f66f67b4d594073342a013bf44e72304fca922
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-30 21:42:34 -0300

    Constantes adicoinadas no arquivo cabecalho.h

--- Flappy Bird/cabecalho.h
+++ Flappy Bird/cabecalho.h
@@ -5,8 +5,27 @@
 #include "sdl2/sdl2.h"
 
 /*  Constantes */
+//  Janela
+#define JANELA_NOME "Flappy Bird"
+#define JANELA_X 0
+#define JANELA_Y 0
+#define JANELA_LARGURA 400
+#define JANELA_ALTURA 600
+//  Fonte
+#define FONTE_ARQUIVO "lazy.ttf"
+#define FONTE_TAMANHO 20
+//  Geral
+#define DELAY 30
 
 /*  Variaveis */
+/*  SDL */
+//  Janela e tela
+SDL_Window *janela = NULL;
+SDL_Renderer *tela = NULL;
+//  Texto
+TTF_Font *fonte = NULL;
+//  Evento
+SDL_Event evento;
 
 /*  Estruturas */
 

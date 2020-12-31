4b68ccfbba208597da08b2e847c5689ca08131c8
Júlio Evêncio <65574850+JulioEvencio@users.noreply.github.com>
2020-12-30 21:36:00 -0300

    Organizando arquivos iniciais do projeto

---
+++ Flappy Bird/cabecalho.h
@@ -0,0 +1,15 @@
+#ifndef CABECALHO_H_INCLUDED
+#define CABECALHO_H_INCLUDED
+
+//  Bibliotecas
+#include "sdl2/sdl2.h"
+
+/*  Constantes */
+
+/*  Variaveis */
+
+/*  Estruturas */
+
+/*  Funcoes */
+
+#endif // CABECALHO_H_INCLUDED
---
+++ Flappy Bird/main.c
@@ -0,0 +1,14 @@
+/*
+    Flappy Bird - versao 1.0
+*/
+
+//  Bibliotecas
+#include "cabecalho.h"
+#include "funcoes.c"
+
+int main(int argc, char *args[])
+{
+    //  Code
+
+    return 0;
+}
